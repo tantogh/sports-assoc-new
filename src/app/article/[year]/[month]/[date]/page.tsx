bd0dee071255a0a315d16eee6da6c4b73cb03191
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-02 14:42:36 +0900

    fix: 新着記事へのダイナミックルーティングを実装
    各新着記事へのダイナミックルーティングを実装しました。
    通し番号指定で各記事へアクセスできるようになりました。

---
+++ src/app/article/[year]/[month]/[date]/page.tsx
@@ -0,0 +1,65 @@
+// /src/app/article/page.tsx
+
+import fs from "fs";
+import path from "path";
+
+import Header from "@/component/common/header/header";
+import Article from "@/component/common/article/article";
+import Title from "@/component/common/title/title";
+
+type ArticleProps = {
+  params: Promise<{
+    year: string;
+    month: string;
+    date: string;
+  }>;
+};
+
+export async function generateStaticParams() {
+  const postsDirectory = path.join(process.cwd(), "content", "articles");
+
+  const posts = fs
+    .readdirSync(postsDirectory, { withFileTypes: true })
+    .flatMap((yearDir) => {
+      if (!yearDir.isDirectory()) return [];
+
+      const yearPath = path.join(postsDirectory, yearDir.name);
+
+      return fs
+        .readdirSync(yearPath, { withFileTypes: true })
+        .flatMap((monthDir) => {
+          if (!monthDir.isDirectory()) return [];
+
+          const monthPath = path.join(yearPath, monthDir.name);
+
+          return fs
+            .readdirSync(monthPath, { withFileTypes: true })
+            .filter((file) => file.isFile() && file.name.endsWith(".md"))
+            .map((file) => ({
+              year: yearDir.name,
+              month: monthDir.name,
+              date: file.name.replace(/\.md$/, ""),
+            }));
+        });
+    });
+
+  return posts.map((post) => ({
+    year: post.year,
+    month: post.month,
+    date: post.date,
+  }));
+}
+
+const  ArticlePage = async({ params }: ArticleProps) => {
+  const { year, month, date } = await params;
+  const filename = `articles/${year}/${month}/${date}.md`;
+  return (
+    <>
+      <Header />
+      <Title title="新着情報" />
+      <Article filename={filename} />
+    </>
+    );
+};
+
+export default ArticlePage;
--- src/component/top/information/information.tsx
+++ src/component/top/information/information.tsx
@@ -9,12 +9,12 @@ const Information = () => {
       <Title title="新着情報" />
       <div className="rounded-xl border border-sky-100/80 bg-white/70 p-4 md:p-6">
         {/* 記事一覧 */}
-        <ArticleHeader baseDir="articles" filePath="/2026/03/2026-03-02.md" href="/article/" />
-        <ArticleHeader baseDir="articles" filePath="/2026/03/2026-03-01.md" href="/article/" />
-        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-03.md" href="/article/" />
-        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-02.md" href="/article/" />
-        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-01.md" href="/article/" />
-        <ArticleHeader baseDir="articles" filePath="/2026/01/2026-01-01.md" href="/article/" />
+        <ArticleHeader baseDir="articles" filePath="/2026/03/2026-03-02.md" href="/article/2026/03/2026-03-02" />
+        <ArticleHeader baseDir="articles" filePath="/2026/03/2026-03-01.md" href="/article/2026/03/2026-03-01" />
+        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-03.md" href="/article/2026/02/2026-02-03" />
+        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-02.md" href="/article/2026/02/2026-02-02" />
+        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-01.md" href="/article/2026/02/2026-02-01" />
+        <ArticleHeader baseDir="articles" filePath="/2026/01/2026-01-01.md" href="/article/2026/01/2026-01-01" />
       </div>
     </section>
   );
