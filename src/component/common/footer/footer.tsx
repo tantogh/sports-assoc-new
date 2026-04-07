79271f21e1c6ac9bc6b0f92e9241987e190b5d66
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-07 14:41:46 +0900

    del: 余分なコードを削除
    Footerコンポーネントの余分な<div>ブロックを削除しました。

--- src/component/common/footer/footer.tsx
+++ src/component/common/footer/footer.tsx
@@ -10,57 +10,55 @@ const Footer = () => {
     <>
       {/* 全体の上下に余白(py-8)を追加して見やすくしています */}
       <div className={`bg-sky-100/80 text-xs xl:text-lg text-center py-8 ${notoSerifJP.className}`}>
-        <div className="flex flex-col items-center">
-          {/* ロゴを横並びにするFlexコンテナを追加 */}
-          <div className="flex justify-center items-center gap-4 mb-6">
-            <Link
-              href="#">
-              <div className="w-8 h-8">
-                <Image
-                  src="images/icon/social/instagram_logo.png"
-                  alt="Instagramのロゴ"
-                  width={5000}
-                  height={5000}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-            </Link>
-            <Link
-              href="#">
-              <div className="w-8 h-8">
-                <Image
-                  src="images/icon/social/facebook_logo.png"
-                  alt="Facebookのロゴ"
-                  width={2084}
-                  height={2084}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-            </Link>
-            <Link
-              href="#">
-              <div className="w-8 h-8">
-                <Image
-                  src="images/icon/social/youtube_icon.png"
-                  alt="YouTubeのロゴ"
-                  width={838}
-                  height={592}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-            </Link>
-          </div>
-
-          <h2 className="text-sm xl:text-lg font-bold mb-2">
-            石川県パラスポーツ協会
-          </h2>
-          <p className="leading-relaxed">
-            〒920-0355<br />
-            石川県金沢市稚日野町北222番 &nbsp; 石川県総合スポーツセンター内<br />
-            TEL: 076-213-6288 &nbsp; FAX: 076-213-6287<br />
-            E-MAIL: <a href="mailto:i-sho-spo@po4.nsk.ne.jp" className="text-blue-600 hover:text-blue-800 hover:underline">i-sho-spo@po4.nsk.ne.jp</a>
-          </p>
+        {/* ロゴを横並びにするFlexコンテナを追加 */}
+        <div className="flex justify-center items-center gap-4 mb-6">
+          <Link
+            href="#">
+            <div className="w-8 h-8">
+              <Image
+                src="images/icon/social/instagram_logo.png"
+                alt="Instagramのロゴ"
+                width={5000}
+                height={5000}
+                className="w-full h-full object-contain"
+              />
+            </div>
+          </Link>
+          <Link
+            href="#">
+            <div className="w-8 h-8">
+              <Image
+                src="images/icon/social/facebook_logo.png"
+                alt="Facebookのロゴ"
+                width={2084}
+                height={2084}
+                className="w-full h-full object-contain"
+              />
+            </div>
+          </Link>
+          <Link
+            href="#">
+            <div className="w-8 h-8">
+              <Image
+                src="images/icon/social/youtube_icon.png"
+                alt="YouTubeのロゴ"
+                width={838}
+                height={592}
+                className="w-full h-full object-contain"
+              />
+            </div>
+          </Link>
         </div>
+
+        <h2 className="text-sm xl:text-lg font-bold mb-2">
+          石川県パラスポーツ協会
+        </h2>
+        <p className="leading-relaxed">
+          〒920-0355<br />
+          石川県金沢市稚日野町北222番 &nbsp; 石川県総合スポーツセンター内<br />
+          TEL: 076-213-6288 &nbsp; FAX: 076-213-6287<br />
+          E-MAIL: <a href="mailto:i-sho-spo@po4.nsk.ne.jp" className="text-blue-600 hover:text-blue-800 hover:underline">i-sho-spo@po4.nsk.ne.jp</a>
+        </p>
       </div>
     </>
   );
