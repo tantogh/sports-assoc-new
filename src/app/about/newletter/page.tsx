cbf40116520b65e61615187fcd3046681e462d82
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-08 14:15:21 +0900

    fix: バグフィックス

---
+++ src/app/about/newletter/page.tsx
@@ -0,0 +1,13 @@
+// src/app/about/newsletter/page.tsx
+
+import Title from "@/component/common/title/title";
+
+const Assoc = () => {
+  return (
+    <>
+      <Title title="協会だより" />
+    </>
+  );
+};
+
+export default Assoc;
