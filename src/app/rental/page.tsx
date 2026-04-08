f4927a5d94a09e161a025c578c7d9b0c4e8e67ed
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-08 15:00:28 +0900

    add: Rentalページを追加

---
+++ src/app/rental/page.tsx
@@ -0,0 +1,13 @@
+// src/app/rental/page.tsx
+
+import Title from "@/component/common/title/title";
+
+const RentalPage = () => {
+  return (
+    <>
+      <Title title="パラスポーツ指導員要請講習会" />
+    </>
+  );
+};
+
+export default RentalPage;
