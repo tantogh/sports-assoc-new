42778496d9a15e32762d0773ad525d41b9d2df3b
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-09 14:59:31 +0900

    add: コンタクトページの内容を追加
    コンタクトページの中身を追加しました。
    TODO: PHPによるメール送信の実装はまだ。

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,6 +12,21 @@ const ContactPage = () => {
   return (
     <>
       <Title title="お問い合わせ" />
+      <form action="confirm.php" method="POST">
+        <div className="p-4">
+          <label htmlFor="name">お名前：</label><br />
+          <input type="text" id="name" name="name" required className="border" />
+        </div>
+        <div className="p-4">
+          <label htmlFor="email">メールアドレス：</label><br />
+          <input type="email" id="email" name="email" required className="border" />
+        </div>
+        <div className="p-4">
+          <label htmlFor="message">お問い合わせ内容：</label><br />
+          <textarea id="message" name="message" required className="border p-16"></textarea>
+        </div>
+        <button type="submit" className="border p-2 m-4 bg-gray-300 rounded-lg">確認画面へ</button>
+      </form>
     </>
   );
 };
