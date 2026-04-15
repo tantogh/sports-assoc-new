93b7b788d107dd9dd2e71c25d67a7cfe98f26553
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-15 11:15:08 +0900

    adj: タイトルの英字のサイズを変更
    Titleコンポーネントの英字サイズを調整しました。
    text-xxsをglobal.cssに定義して、それをTitleコンポーネントの英字に適用しました。

--- src/component/common/title/title.tsx
+++ src/component/common/title/title.tsx
@@ -11,7 +11,7 @@ const Title = ({ subTitle, title }: TitleProps) => {
   return (
     <>
       <div className={`mt-2 p-4 text-center ${notoSerifJP.className}`}>
-        <p className="mb-1 text-xs lg:text-sm font-semibold uppercase tracking-[0.2em] text-sky-600">
+        <p className="text-xxs lg:text-xxs font-semibold uppercase tracking-[0.2em] text-sky-600">
           {subTitle}
         </p>
         <h1 className="text-xl lg:text-2xl font-bold tracking-tight text-slate-900 md:text-5xl">
