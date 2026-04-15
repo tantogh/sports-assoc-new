244a6b13a37edbee440c89dfc42e0cea10029c8c
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-15 10:33:17 +0900

    add: ダウンロードページの内容を追加
    ダウンロードページの内容を実際のものを追加しました。

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -10,9 +10,41 @@ export const metadata: Metadata = {
 
 const DownloadsPage = () => {
   return (
-    <>
-      <Title subTitle="DOWNLOADS" title="様式のダウンロード" />
-    </>
+    <main className="min-h-screen bg-blue-50">
+      <div className="max-w-7xl mx-auto px-4 py-12">
+        <section className="bg-white p-6 rounded-lg shadow-sm border border-blue-100">
+          <Title subTitle="DOWNLOADS" title="ダウンロード" />
+          <table className="w-full border border-gray-400 bg-white text-xs md:text-base">
+            <tbody>
+              <tr>
+                <td className="p-4 border border-gray-400">後援申請・報告書</td>
+                <td className="p-4 border border-gray-400">
+                  <a
+                    href="/downloads/downloads/kouensinseihoukoku2020.doc"
+                    target="_blank"
+                    className="font-bold text-blue-600 underline"
+                  >
+                    ダウンロード
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td className="p-4 border border-gray-400">推薦書</td>
+                <td className="p-4 border border-gray-400">
+                  <a
+                    href="/downloads/downloads/suisensyo.xls"
+                    target="_blank"
+                    className="font-bold text-blue-600 underline"
+                  >
+                    ダウンロード
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </main>
   );
 };
 
