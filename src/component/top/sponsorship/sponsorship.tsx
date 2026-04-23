c61cbdab48900e2198500b2039b7804eda3092a6
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-23 13:35:53 +0900

    mod: ご協賛を中央揃えに変更
    トップページのご協賛を中央揃えに変更しました。

--- src/component/top/sponsorship/sponsorship.tsx
+++ src/component/top/sponsorship/sponsorship.tsx
@@ -7,7 +7,7 @@ export default function Sponsorship() {
     <>
       <section className="w-full py-2">
         <div className="rounded-xl bg-white p-4 md:p-6">
-          <p className={`text-xs xl:text-xl font-bold p-4 ${notoSerifJP.className}`}>
+          <p className={`text-xs xl:text-xl font-bold p-4 text-center ${notoSerifJP.className}`}>
             サントリービバレッジサービス株式会社　様<br />
             株式会社　ゴールドウィン　様
           </p>
