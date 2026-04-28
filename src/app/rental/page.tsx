fc2c268068211d50cb5f5ad53197249526930760
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-04-28 13:59:46 +0900

    add: 用具貸し出しページの骨組みを追加
    用具貸し出しページの骨組みのみを追加しました。
    TODO: スタイリングはまだしていない。
    テーブル形式にするか、リスト形式にするか、要検討です。

--- src/app/rental/page.tsx
+++ src/app/rental/page.tsx
@@ -12,6 +12,45 @@ export default function RentalPage() {
   return (
     <>
       <Title subTitle="RENTAL" title="スポーツ用具貸し出し" />
+      <section className="max-w-2xl mx-auto p-6 bg-white">
+        <p className="p-4">
+          スポーツ用具貸し出しに関する書類はこちらからダウンロードしてください。
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                石川県障害者スポーツ協会用具使用申請書
+              </td>
+              <td>
+                <a href="#">
+                  ダウンロード
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                石川県障害者スポーツ協会用具使用料金表
+              </td>
+              <td>
+                <a href="#">
+                  ダウンロード
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                指導員派遣依頼書
+              </td>
+              <td>
+                <a href="#">
+                  ダウンロード
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </>
   );
 };
