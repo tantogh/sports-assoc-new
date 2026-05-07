1cbe9e36fe18daec2938ace60b52ac4f55942259
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-05-07 11:48:23 +0900

    mod: contentディレクトリ内の構造を変更
    contentディレクトリ内にarchivesとarticlesの2つのディレクトリを追加して、過去記事と現在の記事を住み分けるようにしました。

--- src/component/top/information/information.tsx
+++ src/component/top/information/information.tsx
@@ -7,12 +7,12 @@ export default function Information() {
     <section className="w-full py-2">
       <div className="rounded-xl bg-white p-4 md:p-6">
         {/* 記事一覧 */}
-        <ArticleHeader baseDir="articles" filePath="/2026/03/2026-03-02.md" />
-        <ArticleHeader baseDir="articles" filePath="/2026/03/2026-03-01.md" />
-        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-03.md" />
-        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-02.md" />
-        <ArticleHeader baseDir="articles" filePath="/2026/02/2026-02-01.md" />
-        <ArticleHeader baseDir="articles" filePath="/2026/01/2026-01-01.md" />
+        <ArticleHeader baseDir="articles/information" filePath="/2026/03/2026-03-02.md" />
+        <ArticleHeader baseDir="articles/information" filePath="/2026/03/2026-03-01.md" />
+        <ArticleHeader baseDir="articles/information" filePath="/2026/02/2026-02-03.md" />
+        <ArticleHeader baseDir="articles/information" filePath="/2026/02/2026-02-02.md" />
+        <ArticleHeader baseDir="articles/information" filePath="/2026/02/2026-02-01.md" />
+        <ArticleHeader baseDir="articles/information" filePath="/2026/01/2026-01-01.md" />
       </div>
     </section>
   );
