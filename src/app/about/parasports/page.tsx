01cb95e6a9198f4fe87e711de4b4e5d2aed99323
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-05-14 14:52:04 +0900

    fix: ラッパークラスのタイポ修正
    パラスポーツページのラッパークラスのpx-1をpx-2に変更しました。

--- src/app/about/parasports/page.tsx
+++ src/app/about/parasports/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function Parasports() {
   return (
     <>
-      <div className="max-w-7xl mx-auto px-1 lg:px-4">
+      <div className="max-w-7xl mx-auto px-2 lg:px-4">
         <Title subTitle="PARASPORTS" title="パラスポーツとは" />
       </div>
     </>
