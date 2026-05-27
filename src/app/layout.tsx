ea42d086f24318c93f88ad302bd0b7a6d0d725d4
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-05-27 14:13:18 +0900

    fix: フォント設定をglobals.cssから完全に排除
    フォント設定をglobals.cssから完全に排除しました。
    以後、フォント設定は、tailwindcss.config.mjs、layout.tsx、@/components/utils/fontsを使って変更します。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import { notoSerifJP } from "@/component/utils/fonts/fonts";
+import { notoSerifJP, geistSans, geistMono } from "@/component/utils/fonts/fonts";
 import Header from "@/component/common/header/header";
 import Footer from "@/component/common/footer/footer";
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="jp" className="h-full antialiased scroll-smooth" data-theme="light">
+    <html lang="jp" className={`h-full antialiased scroll-smooth ${geistSans.variable} ${geistMono.variable}`} data-theme="light">
       <body className={`${notoSerifJP.className} min-h-full flex flex-col`}>
         <Header />
         <main className="flex-grow bg-white">{children}</main>
