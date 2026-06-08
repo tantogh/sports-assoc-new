e53a9d195049f8fa6e9f2ea63bac90a4150f8434
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-06-08 11:42:57 +0900

    add: 新しい講演会情報と関連PDFを追加

--- src/component/top/information/information.tsx
+++ src/component/top/information/information.tsx
@@ -6,6 +6,7 @@ export default function Information() {
   return (
     <section className="w-full">
       {/* 記事一覧 */}
+      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-08.md" />
       <ArticleHeader baseDir="articles/information" filePath="/2026/03/2026-03-02.md" />
       <ArticleHeader baseDir="articles/information" filePath="/2026/03/2026-03-01.md" />
       <ArticleHeader baseDir="articles/information" filePath="/2026/02/2026-02-03.md" />
