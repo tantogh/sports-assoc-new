a4326e67ffaff5d236b35def14b1338e0f6e4b1d
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-06-10 13:50:56 +0900

    fix: 過去記事管理ページのコンポーネント名をArchivesに修正しました。

--- src/app/archives/articles/page.tsx
+++ src/app/archives/articles/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   description: "石川県パラスポーツ協会の過去記事のページです。",
 }
 
-export default function ParaSports() {
+export default function Archives() {
   return (
     <>
       <div className="max-w-7xl mx-auto px-2 mb-8 lg:px-4">
