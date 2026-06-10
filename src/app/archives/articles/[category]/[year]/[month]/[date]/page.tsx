04f225d1b3b3f19be0a2d58fbdec78fa660b4bd4
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-06-10 14:46:29 +0900

    fix: 一覧へ戻るボタンホバー時に、マウスカーソルの変化を追加

--- src/app/archives/articles/[category]/[year]/[month]/[date]/page.tsx
+++ src/app/archives/articles/[category]/[year]/[month]/[date]/page.tsx
@@ -49,7 +49,7 @@ export default async function Page({ params }: any) {
       />
       <div className="flex justify-center mb-8">
         <Link href={`/archives/articles#${category}`} passHref>
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors cursor-pointer">
             一覧へ戻る
           </button>
         </Link>
--- src/app/articles/[category]/[year]/[month]/[date]/page.tsx
+++ src/app/articles/[category]/[year]/[month]/[date]/page.tsx
@@ -48,7 +48,7 @@ export default async function Page({ params }: any) {
       />
       <div className="flex justify-center mb-8">
         <Link href={`${meta.url}#${category}`} passHref>
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors cursor-pointer">
             一覧へ戻る
           </button>
         </Link>
