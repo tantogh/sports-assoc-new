fbe84f1942a78a66d090b54de7ce5f114a204b49
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-06-10 13:45:38 +0900

    add: 過去記事のスケルトンを追加。

---
+++ src/app/archives/articles/page.tsx
@@ -0,0 +1,19 @@
+// src/app/clubs/introduction/page.tsx
+import { Metadata } from "next"
+
+import Title from "@/component/common/title/title";
+
+export const metadata: Metadata = {
+  title: "過去記事 | 石川県パラスポーツ協会",
+  description: "石川県パラスポーツ協会の過去記事のページです。",
+}
+
+export default function ParaSports() {
+  return (
+    <>
+      <div className="max-w-7xl mx-auto px-2 mb-8 lg:px-4">
+        <Title subTitle="ARCHIVES" title="過去記事" />
+      </div>
+    </>
+  );
+};
