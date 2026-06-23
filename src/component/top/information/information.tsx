b2fa2aac69fd8880bbbdcd30dade4d7b88e6ecb6
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-06-23 10:04:04 +0900

    fix: 記事マークダウンファイルのファイル名の命名規則を変更
    記事マークダウンファイルのファイル名を「年-月-連番」に変更しました。
    理由：同じ日に複数記事が上げられる可能性があるため。

--- src/component/top/information/information.tsx
+++ src/component/top/information/information.tsx
@@ -6,9 +6,10 @@ export default function Information() {
   return (
     <section className="w-full">
       {/* 記事一覧 */}
-      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-22.md" />
-      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-15.md" />
-      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-08.md" />
+      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-04.md" />
+      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-03.md" />
+      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-02.md" />
+      <ArticleHeader baseDir="articles/information" filePath="/2026/06/2026-06-01.md" />
       <ArticleHeader baseDir="articles/information" filePath="/2026/03/2026-03-02.md" />
       <ArticleHeader baseDir="articles/information" filePath="/2026/03/2026-03-01.md" />
       <ArticleHeader baseDir="articles/information" filePath="/2026/02/2026-02-03.md" />
