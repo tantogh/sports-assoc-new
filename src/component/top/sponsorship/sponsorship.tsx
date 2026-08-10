59836f3fe9eb7598f67691d10383add46a175378
Ken-ichi Yasuda <msredcomet06@gmail.com>
2026-08-10 14:12:17 +0900

    fix: remove unnecessary margin from sponsorship list

--- src/component/top/sponsorship/sponsorship.tsx
+++ src/component/top/sponsorship/sponsorship.tsx
@@ -5,7 +5,7 @@ export default function Sponsorship() {
     <>
       <section className="w-full max-w-2xl mx-auto mb-4 p-2 bg-white rounded-xl shadow-lg">
         <div className="text-xs xl:text-xl p-4">
-          <ul className="list-disc list-inside mb-4">
+          <ul className="list-disc list-inside">
             <li>サントリービバレッジサービス株式会社様</li>
             <li>株式会社　ゴールドウィン様</li>
           </ul>
